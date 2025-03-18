314b8f1e9d5096aa050d8426d4afcae219e8c1a7
1dami <1dami@TartangaLH.eus>
2025-03-18 10:39:17 +0100

    primer commit

---
+++ PRG/RetoFinal/src/modelo/UsuarioDAO.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public interface UsuarioDAO {
+
+	/*public boolean comprobarUsuario(Usuario usuario);
+	public boolean insertarUsuario(Usuario usuario);
+	public Map<String, Usuario> visualizarUsuarios();
+	public boolean eliminarUsuario(String nombre);
+	public boolean modificarUsuario(Usuario usuario);*/
+}
