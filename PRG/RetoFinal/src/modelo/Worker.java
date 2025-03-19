5d5a4bac9e0e663859efc0e5d84ad09bdc9323ba
1dami <1dami@HZ395638.TartangaLH.eus>
2025-03-19 09:32:49 +0100

    worker

--- PRG/RetoFinal/src/modelo/Worker.java
+++ PRG/RetoFinal/src/modelo/Worker.java
@@ -2,5 +2,45 @@ package modelo;
 
 public class Worker {
 
+	private boolean admin;
+	private String user;
+	private String password;
+	
+	public Worker(boolean admin, String user, String password) 
+	{
+		this.admin = admin;
+		this.user = user;
+		this.password = password;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Worker [admin=" + admin + ", user=" + user + ", password=" + password + "]";
+	}
+	
 	
 }
