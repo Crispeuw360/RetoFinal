e016659b62b3aadbf0d49c009112fbf72e567704
Crispeuw360 <igornikolaidis@gmail.com>
2025-03-28 12:30:18 +0100

    more comments for javaDoc

--- PRG/RetoFinal/src/modelo/Client.java
+++ PRG/RetoFinal/src/modelo/Client.java
@@ -1,66 +1,124 @@
 package modelo;
 
+/**
+ * Represents a client in the system.
+ * Each client has a DNI, email, username, and password.
+ * 
+ * @author  Kevin
+ * @version 1.0
+ */
 public class Client {
 
-	private String dni;
-	private String email;
-	private String user_;
-	private String password_;
+    private String dni;
+    private String email;
+    private String user_;
+    private String password_;
 
-	public Client(String dni, String email, String user_, String password_)
-	{
-		this.dni = dni;
-		this.email = email;
-		this.user_ = user_;
-		this.password_ = password_;
-	}
+    /**
+     * Constructs a new Client with the given details.
+     *
+     * @param dni       The client's DNI (identification number).
+     * @param email     The client's email address.
+     * @param user_     The client's username.
+     * @param password_ The client's password.
+     */
+    public Client(String dni, String email, String user_, String password_) {
+        this.dni = dni;
+        this.email = email;
+        this.user_ = user_;
+        this.password_ = password_;
+    }
 
-	public Client() 
-	{
-		this.dni = "";
-		this.email = "";
-		this.user_ = "";
-		this.password_ = "";
-	}
+    /**
+     * Constructs a new Client with empty attributes.
+     */
+    public Client() {
+        this.dni = "";
+        this.email = "";
+        this.user_ = "";
+        this.password_ = "";
+    }
 
-	public String getDni() {
-		return dni;
-	}
-	
-	public void setDni(String dni) {
+    /**
+     * Gets the client's DNI.
+     *
+     * @return The client's DNI.
+     */
+    public String getDni() {
+        return dni;
+    }
 
-		this.dni = dni;
-	}
-	
-	public String getEmail() {
+    /**
+     * Sets the client's DNI.
+     *
+     * @param dni The new DNI for the client.
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 
-		return email;
-	}
-	
-	public void setEmail(String email) {
+    /**
+     * Gets the client's email address.
+     *
+     * @return The client's email.
+     */
+    public String getEmail() {
+        return email;
+    }
 
-		this.email = email;
-	}
-	
-	public String getUser_() 
-	{
-		return user_;
-	}
-	
-	public void setUser_(String user_) {
-		this.user_ = user_;
-	}
-	
-	public String getPassword_() {
-		return password_;
-	}
-	
-	public void setPassword_(String password_) {
-		this.password_ = password_;
-	}
-	
-	@Override
-	public String toString() {
-		return "Client [dni=" + dni + ", email=" + email + ", user_=" + user_ + ", password_=" + password_ + "]";
-	}
+    /**
+     * Sets the client's email address.
+     *
+     * @param email The new email for the client.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets the client's username.
+     *
+     * @return The client's username.
+     */
+    public String getUser_() {
+        return user_;
+    }
+
+    /**
+     * Sets the client's username.
+     *
+     * @param user_ The new username for the client.
+     */
+    public void setUser_(String user_) {
+        this.user_ = user_;
+    }
+
+    /**
+     * Gets the client's password.
+     *
+     * @return The client's password.
+     */
+    public String getPassword_() {
+        return password_;
+    }
+
+    /**
+     * Sets the client's password.
+     *
+     * @param password_ The new password for the client.
+     */
+    public void setPassword_(String password_) {
+        this.password_ = password_;
+    }
+
+    /**
+     * Returns a string representation of the client.
+     *
+     * @return A string containing the client's DNI, email, username, and password.
+     */
+    @Override
+    public String toString() {
+        return "Client [dni=" + dni + ", email=" + email + ", user_=" + user_ + ", password_=" + password_ + "]";
+    }
 }
+
--- PRG/RetoFinal/src/modelo/Model.java
+++ PRG/RetoFinal/src/modelo/Model.java
@@ -1,75 +1,149 @@
 package modelo;
 
+/**
+ * Represents a car model in the system.
+ * Each car model has a name, brand, stock quantity, price, and associated car dealership.
+ * 
+ * @author Igor
+ * @version 1.0
+ */
 public class Model {
 
-	private String name_model;
-	private String mark;
-	private int stock;
-	private double price;
-	private int id_car_dealer;
-	
-	public Model(String name_model, String mark, int stock, double price, int id_car_dealer) {
-		super();
-		this.name_model = name_model;
-		this.mark = mark;
-		this.stock = stock;
-		this.price = price;
-		this.id_car_dealer = id_car_dealer;
-	}
-	
-	public Model() {
-		super();
-		this.name_model = "";
-		this.mark = "";
-		this.stock = 0;
-		this.price = 0;
-		this.id_car_dealer = 0;
-	}
+    private String name_model;
+    private String mark;
+    private int stock;
+    private double price;
+    private int id_car_dealer;
 
-	public String getName_model() {
-		return name_model;
-	}
+    /**
+     * Constructs a new Model with the specified name, brand, stock quantity, price, and car dealership ID.
+     *
+     * @param name_model    The name of the car model.
+     * @param mark          The brand of the car model.
+     * @param stock         The stock quantity of the car model.
+     * @param price         The price of the car model.
+     * @param id_car_dealer The ID of the associated car dealership.
+     */
+    public Model(String name_model, String mark, int stock, double price, int id_car_dealer) {
+        super();
+        this.name_model = name_model;
+        this.mark = mark;
+        this.stock = stock;
+        this.price = price;
+        this.id_car_dealer = id_car_dealer;
+    }
 
-	public void setName_model(String name_model) {
-		this.name_model = name_model;
-	}
+    /**
+     * Constructs a new Model with default values (empty name, empty brand, 0 stock, 0 price, and car dealership ID 0).
+     */
+    public Model() {
+        super();
+        this.name_model = "";
+        this.mark = "";
+        this.stock = 0;
+        this.price = 0;
+        this.id_car_dealer = 0;
+    }
 
-	public String getMark() {
-		return mark;
-	}
+    /**
+     * Gets the name of the car model.
+     *
+     * @return The name of the car model.
+     */
+    public String getName_model() {
+        return name_model;
+    }
 
-	public void setMark(String mark) {
-		this.mark = mark;
-	}
+    /**
+     * Sets the name of the car model.
+     *
+     * @param name_model The new name for the car model.
+     */
+    public void setName_model(String name_model) {
+        this.name_model = name_model;
+    }
 
-	public int getStock() {
-		return stock;
-	}
+    /**
+     * Gets the brand of the car model.
+     *
+     * @return The brand of the car model.
+     */
+    public String getMark() {
+        return mark;
+    }
 
-	public void setStock(int stock) {
-		this.stock = stock;
-	}
+    /**
+     * Sets the brand of the car model.
+     *
+     * @param mark The new brand for the car model.
+     */
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    /**
+     * Gets the stock quantity of the car model.
+     *
+     * @return The stock quantity of the car model.
+     */
+    public int getStock() {
+        return stock;
+    }
 
-	public void setPrice(double price) {
-		this.price = price;
-	}
+    /**
+     * Sets the stock quantity of the car model.
+     *
+     * @param stock The new stock quantity for the car model.
+     */
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 
-	public int getId_car_dealer() {
-		return id_car_dealer;
-	}
+    /**
+     * Gets the price of the car model.
+     *
+     * @return The price of the car model.
+     */
+    public double getPrice() {
+        return price;
+    }
 
-	public void setId_car_dealer(int id_car_dealer) {
-		this.id_car_dealer = id_car_dealer;
-	}
+    /**
+     * Sets the price of the car model.
+     *
+     * @param price The new price for the car model.
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
-	@Override
-	public String toString() {
-		return "Model [name_model=" + name_model + ", mark=" + mark + ", stock=" + stock + ", price=" + price
-				+ ", id_car_dealer=" + id_car_dealer + "]";
-	}
-	
+    /**
+     * Gets the ID of the car dealership associated with the car model.
+     *
+     * @return The ID of the car dealership.
+     */
+    public int getId_car_dealer() {
+        return id_car_dealer;
+    }
+
+    /**
+     * Sets the ID of the car dealership associated with the car model.
+     *
+     * @param id_car_dealer The new ID of the car dealership.
+     */
+    public void setId_car_dealer(int id_car_dealer) {
+        this.id_car_dealer = id_car_dealer;
+    }
+
+    /**
+     * Returns a string representation of the car model.
+     *
+     * @return A string containing the name, brand, stock, price, and associated car dealership ID.
+     */
+    @Override
+    public String toString() {
+        return "Model [name_model=" + name_model + ", mark=" + mark + ", stock=" + stock + ", price=" + price
+                + ", id_car_dealer=" + id_car_dealer + "]";
+    }
 }
+
